332c89c90991a690e906589080359b81b5254ca1
Alexius337 <almyro@gmail.com>
2022-10-05 23:03:01 -0700

    filled in bookstore test

---
+++ tests/bookstore.test.js
@@ -0,0 +1,17 @@
+const { chromium } = require('playwright');
+
+describe('UI tests for bookstore using playwright', async() => {
+
+    beforeAll(async() =>{
+
+    });
+
+    afterAll(async() =>{
+
+    });
+
+    test('Should load page', async() =>{
+
+    });
+
+});
